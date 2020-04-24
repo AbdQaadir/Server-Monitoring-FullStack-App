c2cb1a01e6f7e258bc51159d2961391eb1a64051
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-04-24 12:22:30 +0100

    Fixes dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
-
-const { Pool } = require('pg');
+var Sequelize = require('sequelize');
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const conStr = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connection = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, {
+    define: {
+        timestamps: false
+    }
+})
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : conStr
-});
 
-module.exports = { pool };
+const User = connection.define('users', {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+})
+
+module.exports = {connection, User};
