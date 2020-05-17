94cd6a112e56249dd31775cfa4faf2539cb20b65
Lateef Quadri <Lateef9816@gmail.com>
2020-05-17 18:36:53 +0100

    Fixes

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
       "database": "nodelogin",
       "host": "127.0.0.1",
       "dialect": "postgres",
-      "port": 8000
+      "port": 5432
       // "operatorsAliases": false
     },
     "test": {
@@ -14,7 +14,7 @@ module.exports = {
       "database": "nodelogin",
       "host": "127.0.0.1",
       "dialect": "postgres",
-      "port": 5432
+      "port": 8000
     },
     "production": {
       "username": "root",
